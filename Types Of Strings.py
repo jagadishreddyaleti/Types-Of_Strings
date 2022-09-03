0f2b16f9d8f6b8cf088ab4f5d8f6a4b5ee93f073
jagadishreddyaleti <111480451+jagadishreddyaleti@users.noreply.github.com>
2022-09-03 14:52:42 +0530

    Add files via upload

---
+++ Types Of Strings.py
@@ -0,0 +1,16 @@
+a='jagadishreddy'
+print(type(a))  # strings
+
+
+''' raw string'''
+
+print('python\new')
+print('python\\new')
+print(r'python\new')  # raw string is used for escape sequences
+
+
+# ''' fast string'''
+
+a='python'
+print('hello {a}') 
+print(f'hello {a}') # fast string is used for  filling the placeholders
